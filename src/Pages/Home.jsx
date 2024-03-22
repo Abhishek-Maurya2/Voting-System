10f5aab9a0457170cc3adc7a24a508bd563dcc1b
Abhishek-Maurya2 <208akmaurya@hmail.com>
2024-03-23 00:09:42 +0530

    dashboard

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import "./home.css";
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <Navbar />
-      <div className="conatiner">
-        <div className="left"></div>
-        <div className="right"></div>
+      <div className="top">
+        {/* profile card */}
+        <div className="aside">
+          <div className="profile-card">
+            <div className="profile-img">
+              <img
+                className="profile-img"
+                src="https://avatars.githubusercontent.com/u/120308980?v=4"
+                alt="profile"
+              />
+            </div>
+            <div className="profile-content">
+              <p className="profile-name">John Doe</p>
+              <p className="profile-number">43112328</p>
+            </div>
+          </div>
+          <div className="create-poll">
+            <p>Create a new Poll</p>
+            <button className="create-poll-btn">Create Poll</button>
+          </div>
+        </div>
+        <div className="dashboard">
+          <p className="dash-heading">Active Polls</p>
+        </div>
       </div>
     </div>
   );
